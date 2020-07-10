1b963eea8d4ee20746a27f29491ed3e097422f75
Константин Яник <zybr.kon@gmail.com>
2020-07-10 20:58:35 +0300

    half findFile

--- gamefiles.cpp
+++ gamefiles.cpp
@@ -1,11 +1,28 @@
 #include "gamefiles.h"
 #include <QDebug>
+#include <QVBoxLayout>
+
 GameFiles::GameFiles(QWidget *parent) : QWidget(parent)
 {
-    firstTextEdit = new QTextEdit("l", this);
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(firstTextEdit);
-    layout->setContentsMargins(0,0,0,0);
+    fileModel = new QFileSystemModel(this);
+    fileModel->setRootPath("");
+
+    mainTree = new QTreeView(this);
+    mainTree->setModel(fileModel);
+
+    favouritesTree = new QTreeView(this);
+    //QVBoxLayout *layout = new Q
+    splitter = new QSplitter(this);
+    //favouritesTree->setModel(new Model);
+    splitter->setOrientation(Qt::Vertical);
+    splitter->setHandleWidth(1);
+
+    splitter->addWidget(mainTree);
+    splitter->addWidget(favouritesTree);
+    //layout->addWidget(firstTextEdit);
+    //layout->setContentsMargins(0,0,0,0);
     //setLayout(layout);
+
+     //setRootIsDecorated(false) // for two tree
 
 }
--- gamefiles.h
+++ gamefiles.h
@@ -4,10 +4,17 @@
 #include <QWidget>
 #include <QTextEdit>
 #include <QVBoxLayout>
+#include <QSplitter>
+#include <QFileSystemModel>
+#include <QTreeView>
 
 class GameFiles : public QWidget
 {
     Q_OBJECT
+    QSplitter *splitter;
+    QFileSystemModel *fileModel;
+    QTreeView *mainTree;
+    QTreeView *favouritesTree;
     QTextEdit *firstTextEdit;
 public:
     explicit GameFiles(QWidget *parent = nullptr);

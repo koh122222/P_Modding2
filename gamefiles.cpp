793e5984a4b8aaa4951e25a9d6a8e9baa6f6a06f
Константин Яник <zybr.kon@gmail.com>
2020-07-11 23:42:53 +0300

    build file system

--- gamefiles.cpp
+++ gamefiles.cpp
@@ -5,24 +5,40 @@
 GameFiles::GameFiles(QWidget *parent) : QWidget(parent)
 {
     fileModel = new QFileSystemModel(this);
+    //fileModel->
     fileModel->setRootPath("");
 
     mainTree = new QTreeView(this);
     mainTree->setModel(fileModel);
+    mainTree->hideColumn(1);
+    mainTree->hideColumn(2);
+    mainTree->hideColumn(3);
+    //fileModel->data();
 
+    favouritesModel = new QFileSystemModel(this);
     favouritesTree = new QTreeView(this);
-    //QVBoxLayout *layout = new Q
+    favouritesTree->setModel(favouritesModel);
+
+    connect(mainTree, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(openFiles(QModelIndex)));
+
+
     splitter = new QSplitter(this);
+    layout = new QVBoxLayout(this);
     //favouritesTree->setModel(new Model);
     splitter->setOrientation(Qt::Vertical);
     splitter->setHandleWidth(1);
-
     splitter->addWidget(mainTree);
     splitter->addWidget(favouritesTree);
-    //layout->addWidget(firstTextEdit);
-    //layout->setContentsMargins(0,0,0,0);
-    //setLayout(layout);
-
+    layout->addWidget(splitter);
+    layout->setContentsMargins(0,0,0,0);
+    setLayout(layout);
+    //qDebug() << mainTree[0];
      //setRootIsDecorated(false) // for two tree
 
+}
+
+void GameFiles::openFiles(const QModelIndex& index)
+{
+    favouritesTree->
+    qDebug() << "openF";
 }
--- gamefiles.h
+++ gamefiles.h
@@ -7,19 +7,27 @@
 #include <QSplitter>
 #include <QFileSystemModel>
 #include <QTreeView>
+#include <QModelIndex>
 
 class GameFiles : public QWidget
 {
     Q_OBJECT
+    QVBoxLayout *layout;
     QSplitter *splitter;
+
     QFileSystemModel *fileModel;
+    QFileSystemModel *favouritesModel;
+
     QTreeView *mainTree;
     QTreeView *favouritesTree;
-    QTextEdit *firstTextEdit;
 public:
     explicit GameFiles(QWidget *parent = nullptr);
 
 signals:
+
+public slots:
+    void openFiles(const QModelIndex& index);
+
 
 };
 
